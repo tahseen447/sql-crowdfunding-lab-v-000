454e7bc4aee8897c79aaaa31997c1d9ae0d45ce8
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-08 23:08:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"select projects.category from projects join pledges on pledges.project_id = projects.id where projects.category='music'"
+"select projects.category from projects join pledges on pledges.project_id = projects.id where projects.category='music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
